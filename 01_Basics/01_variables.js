51432bb68c32d922a492626fed8d0355efaecc17
notVivekG <notvivekkushwaha@gmail.com>
2025-09-18 11:24:32 +0000

    new files added in basics

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accoutId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// const values cannot be changed
+
+/*
+Prefer not to use var
+because of block scope 
+
+if value is not assigned then after printing the variable , it will display undefined
+*/
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert ( 3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Vivek");
+
+
+let name = "Vivek"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean  => true/false
+// null => standalone value (empty)
+// undefined => not assigned variable 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "vivek");
+
