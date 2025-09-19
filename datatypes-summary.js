74ea3a16630e4c9ce67218ff906edc8e13b40463
notVivekG <notvivekkushwaha@gmail.com>
2025-09-19 11:51:56 +0000

    ep09

---
+++ datatypes-summary.js
@@ -0,0 +1,54 @@
+// Primitive 
+
+// 7 types : String , Boolean, Null , undefined , Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggesIn = false
+const outsideTemp = null 
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3235161454232453452n // converted to bigInt
+
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "vivek",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction);
+
+// Return Type of Variables of Js
+
+// 1) Primitive Datatypes
+//     Number => number 
+//     String => string 
+//     Boolean => boolean 
+//     null => object 
+//     undefined => undefined 
+//     Symbol => symbol 
+//     BigInt => bigint 
+
+// 2) Non-primitive Datatypes 
+//     Arrays => object 
+//     Function => function 
+//     Object => object 
+
